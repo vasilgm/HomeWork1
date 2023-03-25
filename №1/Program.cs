4028d1db05ede119d3a6d9c676aa499aee345157
Vasil <vasilgm@yandex.ru>
2023-03-25 22:53:30 +0300

    №1.2

--- №1/Program.cs
+++ №1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число:");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int b = int.Parse(Console.ReadLine());
+
+if ( a > b )
+{
+Console.Write("max = ");
+Console.WriteLine(a);
+
+Console.Write("min = ");
+Console.WriteLine(b);
+}
+else
+{
+Console.Write("max = ");
+Console.WriteLine(b);
+
+Console.Write("min = ");
+Console.WriteLine(a);
+}

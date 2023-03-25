c17e35fb542abdb16a5503f9bcbe6f4c1f49a790
Vasil <vasilgm@yandex.ru>
2023-03-25 23:55:56 +0300

    №4

---
+++ №4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число N:");
+int N = int.Parse(Console.ReadLine());
+int number = 0;
+while (number < N-1)
+{
+number = number + 2;
+Console.Write( number );
+Console.Write( " " );
+}

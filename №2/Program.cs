80739f665caf4467c4dc2d256bc4f098c804d450
Vasil <vasilgm@yandex.ru>
2023-03-25 23:30:22 +0300

    №2

---
+++ №2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите первое число:");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите третье число:");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (a > max) 
+{
+    max = a;
+}
+if (b>max) 
+{
+    max = b;
+}
+if (c>max) 
+{
+    max = c;
+}
+Console.Write("max = ");
+Console.WriteLine(max);

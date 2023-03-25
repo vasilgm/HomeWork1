e545628d2645506f48cc02d47d95fb51013b70a8
Vasil <vasilgm@yandex.ru>
2023-03-25 23:44:13 +0300

    №3

---
+++ №3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число:");
+int a = int.Parse(Console.ReadLine());
+
+int b = a % 2;
+if (b == 0)
+{
+Console.WriteLine("Число делится на 2");
+}
+else
+{
+Console.WriteLine("Число не делится на 2");
+}
